e413790248a1e3d1d0b16d11015f37b8775a69e1
Yanghaiqiang <XiaoGuaiShouChengJiuLeAoTeMan@hotmail.com>
2021-05-27 01:13:26 +0800

    Records类型

---
+++ RecordsInCSharp/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using RecordsInCSharp;
+//C#9 添加记录类型，记录是一种类
+namespace RecordsInCSharp
+{
+    public record Person(string FirstName, string LastName)
+    {
+        public string[] PhoneNumbers { get; init; }
+    }
+}
+
+internal class RecordTest
+{
+    internal static void PersonTest()
+    {
+        Person person1 = new Person("Nancy", "Davolio") { PhoneNumbers = new string[1] };
+        Console.WriteLine($"person1:{person1}");
+
+        Person person2 = person1 with { FirstName = "John" };
+        Console.WriteLine($"person2:{person2}");
+
+        Console.WriteLine(@"person1 with {FirstName = 'John'}," + $" person1==person2:{person1 == person2}");
+
+        person2 = person1 with { PhoneNumbers = new string[1] };
+        Console.WriteLine($"person2:{person2}");
+
+        Console.WriteLine($"person1==person2:{person1 == person2}");
+
+        person2 = person1 with { };
+        Console.WriteLine("person1 with { } person1==person2:" + $"{person1 == person2}");
+    }
+}
---
+++ RecordsInCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+RecordTest.PersonTest();
+
+//C#9.0 可以省略下面的Main方法
+//Console.WriteLine("Hello World!");
+
+
+//可以省略
+/*namespace RecordsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}*/
+
+
+/*
+从C#7.1开始，也可以返回类型。void int Task Task<int>
+public static void Main() { }
+public static int Main() { }
+public static void Main(string[] args) { }
+public static int Main(string[] args) { }
+public static async Task Main() { }
+public static async Task<int> Main() { }
+public static async Task Main(string[] args) { }
+public static async Task<int> Main(string[] args) { }
+*/
