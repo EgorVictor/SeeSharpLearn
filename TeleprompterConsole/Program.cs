4ac3877c114dc71fc7a5d11e4f9473936422bceb
Yanghaiqiang <XiaoGuaiShouChengJiuLeAoTeMan@hotmail.com>
2021-06-01 22:03:13 +0800

    控制台程序

---
+++ TeleprompterConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TeleprompterConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RunTeleprompter().Wait();
+        }
+
+
+        private static async Task ShowTeleprompter(TeleprompterConfig config)
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var word in words)
+            {
+                Console.Write(word);
+                if (!string.IsNullOrWhiteSpace(word)) await Task.Delay(config.DelayInMilliseconds);
+            }
+        }
+
+        private static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using var reader = File.OpenText(file);
+            while ((line = reader.ReadLine()) != null)
+            {
+                var lineLength = 0;
+                var words = line.Split(' ');
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                    lineLength += word.Length + 1;
+                    if (lineLength > 70)
+                    {
+                        yield return Environment.NewLine;
+                        lineLength = 0;
+                    }
+                }
+
+                yield return Environment.NewLine;
+            }
+        }
+
+
+        private static async Task GetInput(TeleprompterConfig config)
+        {
+            void Work()
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                        config.UpdateDelay(-10);
+                    else if (key.KeyChar == '<')
+                        config.UpdateDelay(10);
+                    else if (key.Modifiers == ConsoleModifiers.Control && key.Key == ConsoleKey.X) config.SetDone();
+                } while (!config.Done);
+            }
+
+            await Task.Run(Work);
+        }
+
+        private static async Task RunTeleprompter()
+        {
+            var config = new TeleprompterConfig();
+            var displayTask = ShowTeleprompter(config);
+            var speedTask = GetInput(config);
+            await Task.WhenAny(displayTask, speedTask);
+        }
+    }
+}
---
+++ TeleprompterConsole/TelePrompterConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace TeleprompterConsole
+{
+    internal class TeleprompterConfig
+    {
+        internal int DelayInMilliseconds { get; private set; } = 200;
+
+        internal void UpdateDelay(int increment)
+        {
+            var newDelay= Min(DelayInMilliseconds + increment, 1000);
+            newDelay = Max(newDelay, 20);
+            DelayInMilliseconds = newDelay;
+        }
+
+        public bool Done { get; private set; }
+
+        public void SetDone()
+        {
+            Done = true;
+        }
+    }
+}
